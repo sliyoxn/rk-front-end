88c9ae52668995d8c2ccdd7bad7734d99348d736
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-06-06 19:42:41 +0800

    <feat>:user

---
+++ src/components/HomeChild/User.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="UserView">
+		<div class="opt-container">
+			<el-button type="primary" @click="addUser">增加用户</el-button>
+			<el-button type="primary" @click="delUser">删除用户</el-button>
+		</div>
+
+		<el-table :data="tableData" style="width: 100%" >
+			<el-table-column align="center" prop="uid" label="id"></el-table-column>
+			<el-table-column align="center" prop="name" label="姓名" ></el-table-column>
+			<el-table-column align="center" prop="phone" label="电话"></el-table-column>
+			<el-table-column align="center" prop="email" label="邮箱"></el-table-column>
+			<el-table-column align="center" label="权限">
+				<template slot-scope="scope">
+					{{scope.row.identity === 1 ? '普通用户' : '管理员'}}
+				</template>
+			</el-table-column>
+		</el-table>
+	</div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({
+	components: {},
+})
+export default class User extends Vue {
+	private tableData = [{
+		uid : '1',
+		name : 'SakuraSnow',
+		phone : '123456789',
+		email : "123456789@qq.com",
+		identity : 1
+	}, {
+		uid : '2',
+		name : 'Snow',
+		phone : '147852369',
+		email : "147852369@qq.com",
+		identity : 2
+	}];
+	private addUser() {
+
+	}
+	private delUser() {
+
+	}
+}
+</script>
+
+<style scoped lang="scss">
+	.UserView {
+		.opt-container {
+			margin-top: 30px;
+			margin-bottom: 30px;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,100 @@
 <template>
-	<div class="home">
-
+	<div class="HomeView">
+		<div class="menu-container">
+			<el-row class="tac">
+				<el-col>
+					<h5 class="menu-text">菜单</h5>
+					<el-menu
+						default-active="1"
+						class="el-menu-vertical-demo"
+						@open="handleOpen"
+						@close="handleClose"
+						@select="handleChange"
+						background-color="#545c64"
+						text-color="#fff"
+						active-text-color="#ffd04b"
+					>
+						<el-menu-item index="1">
+							<i class="el-icon-location"></i>
+							<span>人员</span>
+						</el-menu-item>
+						<el-menu-item index="2">
+							<i class="el-icon-menu"></i>
+							<span slot="title">项目</span>
+						</el-menu-item>
+						<el-menu-item index="3">
+							<i class="el-icon-document"></i>
+							<span slot="title">导航三</span>
+						</el-menu-item>
+						<el-menu-item index="4">
+							<i class="el-icon-setting"></i>
+							<span slot="title">导航四</span>
+						</el-menu-item>
+					</el-menu>
+				</el-col>
+			</el-row>
+		</div>
+		<div class="content-container">
+			<User v-if="index === 1"/>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import User from "@/components/HomeChild/User.vue";
 
 @Component({
-	components: {},
+	components: {User},
 })
 export default class Home extends Vue {
 
+	private index : number = 1;
+
+	private handleOpen() {
+		console.log("?")
+	}
+	private handleClose() {
+
+	}
+	private handleChange(e : any) {
+		console.log(e)
+	}
 }
 </script>
+
+<style lang="scss">
+	.HomeView {
+		height: 100%;
+		.menu-text {
+			background-color : #545c64;
+			color : #fff;
+			margin: 0;
+			padding: 20px 0;
+		}
+		display: flex;
+		flex-direction: row;
+		.menu-container {
+			flex: 0 0;
+			width: 180px;
+			height: 100%;
+			.tac {
+				height: 100%;
+				 > .el-col {
+					 height: 100%;
+					 background: #545c64;
+				}
+			}
+		}
+		.content-container {
+			flex: 1 1 auto;
+		}
+		//.el-menu-vertical-demo  {
+		//	//transform: translate(-30px);
+		//}
+	}
+</style>
+
+<style lang="scss">
+
+</style>
